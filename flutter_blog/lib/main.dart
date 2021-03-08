2089b45fe7dd39f98346dd957abd4f743d4e5b94
suihongwei <suihongwei@konka.com>
2021-03-09 00:48:42 +0800

    feat:web添加loading页面

--- flutter_blog/lib/main.dart
+++ flutter_blog/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '贰拾肆的宠物_8kEatRadish',
       theme: ThemeData(
         // This is the theme of your application.
         //
