10a73323264e89271991f5401c0d0f3e2b164b3c
suihongwei <suihongwei@konka.com>
2021-03-09 18:19:37 +0800

    fix:添加flutter_markdown&添加url_launcher

---
+++ flutter_blog/lib/blog_route.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+
+import 'home/view.dart';
+
+/////////////////////////////////////////////
+/// 文件： blog_route.dart
+/// 描述： 博客页面路由
+/// 作者： Suihongwei 2021/3/9
+/////////////////////////////////////////////
+class RouteConfig {
+  //主页
+  static final String familyBoardHome = "blog_home";
+
+  static final List<GetPage> getPages = [
+    GetPage(name: familyBoardHome, page: () => HomePage())
+  ];
+}
