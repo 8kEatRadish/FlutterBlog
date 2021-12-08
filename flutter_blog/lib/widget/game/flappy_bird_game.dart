86668fb78ca160adc76b88f9bd1151f50c5f6ecf
suihongwei <18845795735@163.com>
2021-12-08 18:07:29 +0800

    feat: 添加小鸟

--- flutter_blog/lib/widget/game/flappy_bird_game.dart
+++ flutter_blog/lib/widget/game/flappy_bird_game.dart
@@ -4,9 +4,8 @@ import 'dart:ui';
 import 'package:flame/animation.dart' as at;
 
 import 'package:flame/components/animation_component.dart';
+import 'package:flame/components/component.dart';
 import 'package:flame/game.dart';
-import 'package:flame/particle.dart';
-import 'package:flame/particles/computed_particle.dart';
 import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -16,6 +15,8 @@ class FlappyBirdGame extends BaseGame {
 
   Random rnd = Random();
   Function randomOffset;
+
+  SpriteComponent bg;
 
   FlappyBirdGame() {
     randomOffset = () => Offset(
@@ -28,6 +29,9 @@ class FlappyBirdGame extends BaseGame {
     player = AnimationComponent(
         64.0, 64.0, new at.Animation.spriteList(sprites, stepTime: 0.1));
 
+    bg =  SpriteComponent.fromSprite(Get.width, Get.height, Sprite('bg_night.png'));
+    bg.x = 0;
+    bg.y = 0;
     // 指定在屏幕上的坐标
     player.x = Get.width / 2; // 若不指定，默认为0
     player.y = Get.height / 2; // 若不指定，默认为0
@@ -44,6 +48,7 @@ class FlappyBirdGame extends BaseGame {
   @override
   void render(Canvas canvas) {
     // 仅当图片加载完成，x，y和宽高不为0时才会进行渲染
+    bg.render(canvas);
     player.render(canvas);
     super.render(canvas);
   }
